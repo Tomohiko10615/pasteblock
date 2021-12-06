ed1656ea996865bd52582cf977d50c2a0af9a543
Tomohiko <pasteblock2021@gmail.com>
2021-12-06 16:48:09 -0500

    auto calculate months

--- src/main/java/com/pasteblock/pasteblock/app/controllers/MobileRestController.java
+++ src/main/java/com/pasteblock/pasteblock/app/controllers/MobileRestController.java
@@ -3,7 +3,6 @@ package com.pasteblock.pasteblock.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +20,5 @@ public class MobileRestController {
 	@GetMapping({"/clientes/listar", "/clientes"})
 	public List<Cliente> index() {
 		return clienteDao.findAll();
+	}
 }
