cb1865b551ca74bf3baa8e41657447226c93e69b
Tomohiko <pasteblock2021@gmail.com>
2021-12-11 08:19:23 -0500

    contratos

--- src/main/java/com/pasteblock/pasteblock/app/controllers/MainController.java
+++ src/main/java/com/pasteblock/pasteblock/app/controllers/MainController.java
@@ -1,13 +1,15 @@
 package com.pasteblock.pasteblock.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 	
 	@GetMapping({"/", "/home", "/index"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("accion", "Home");
 		return "index";
 	}
 }
