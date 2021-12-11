4217c699b13cfd7d90ba604f97f900d168da202f
Tomohiko <pasteblock2021@gmail.com>
2021-12-11 12:20:01 -0500

    mensajes

---
+++ src/main/java/com/pasteblock/pasteblock/app/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.pasteblock.pasteblock.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
+	
+	@Autowired
+	public void configurerGlobal (AuthenticationManagerBuilder builder) throws Exception {
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		builder.inMemoryAuthentication()
+		.withUser(users.username("admin").password("Lamerced.17").roles("ADMIN"));
+	}
+}
